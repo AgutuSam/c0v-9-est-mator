4342b73a2d2d119e8dfe393bb52885240665dc6f
AgutuSam <sammyhardcell@gmail.com>
2020-04-12 23:48:58 +0300

    state one to three

--- src/estimator.php
+++ src/estimator.php
@@ -2,5 +2,98 @@
 
 function covid19ImpactEstimator($data)
 {
-  return $data;
+    $stats = jaysonDecode($data);
+    $stats = json_decode($stats, true);
+    // echo $stats['region']['avgAge'];
+    $impactCurrentlyInfected = $stats['reportedCases'] * 10;
+    $severeImpactCurrentlyInfected = $stats['reportedCases'] * 50;
+
+    if ($stats['periodType'] == 'days') {
+        $factor = intval($stats['timeToElapse'] / 3);
+    } elseif ($stats['periodType'] == 'weeks') {
+        $factor = intval(($stats['timeToElapse'] * 7) / 3);
+    } elseif ($stats['periodType'] == 'months') {
+        $factor = intval(($stats['timeToElapse'] * 30) / 3);
+    }
+
+    $impactInfectionsByRequestedTime = $impactCurrentlyInfected * (pow(2, $factor));
+    $severeImpactInfectionsByRequestedTime = $severeImpactCurrentlyInfected * (pow(2, $factor));
+
+    $impactSevereCasesByRequestedTime = intval((15 * $impactInfectionsByRequestedTime) / 100);
+    $severeImpactSevereCasesByRequestedTime = intval((15 * $severeImpactInfectionsByRequestedTime) / 100);
+
+    $expectedHospitalBedsByRequestedTime = intval((35 * $stats['totalHospitalBeds']) / 100);
+
+    $impactHospitalBedsByRequestedTime = intval(
+        $expectedHospitalBedsByRequestedTime - $impactSevereCasesByRequestedTime
+    );
+    $severeImpactHospitalBedsByRequestedTime = intval(
+        $expectedHospitalBedsByRequestedTime - $severeImpactSevereCasesByRequestedTime
+    );
+
+    $impactCasesForICUByRequestedTime = intval((5 * $impactInfectionsByRequestedTime) / 100);
+    $severeImpactCasesForICUByRequestedTime = intval((5 * $severeImpactInfectionsByRequestedTime) / 100);
+
+    $impactCasesForVentilatorsByRequestedTime = intval((2 * $impactInfectionsByRequestedTime) / 100);
+    $severeImpactCasesForVentilatorsByRequestedTime = intval((2 * $severeImpactInfectionsByRequestedTime) / 100);
+
+    if ($stats['periodType'] == 'days') {
+        $days = intval($stats['timeToElapse']);
+    } elseif ($stats['periodType'] == 'weeks') {
+        $days = intval($stats['timeToElapse'] * 7);
+    } elseif ($stats['periodType'] == 'months') {
+        $days = intval($stats['timeToElapse'] * 30);
+    }
+
+    $impactDollarsInFlight = intval(
+        (
+            (
+                $impactInfectionsByRequestedTime * $stats['region']['avgDailyIncomePopulation']
+                ) * $stats['region']['avgDailyIncomeInUSD']
+                ) * $days
+    );
+    $severeImpactDollarsInFlight = intval(
+        (
+            (
+                $severeImpactInfectionsByRequestedTime * $stats['region']['avgDailyIncomePopulation']
+                ) * $stats['region']['avgDailyIncomeInUSD']
+                ) * $days
+    );
+
+    $stats = json_encode($stats);
+    $estimate = '{
+        data: '.$data.',
+        impact: {
+            currentlyInfected: '.$impactCurrentlyInfected.',
+            infectionsByRequestedTime: '.$impactInfectionsByRequestedTime.',
+            severeCasesByRequestedTime: '.$impactSevereCasesByRequestedTime.',
+            hospitalBedsByRequestedTime: '.$impactHospitalBedsByRequestedTime.',
+            casesForICUByRequestedTime: '.$impactCasesForICUByRequestedTime.',
+            casesForVentilatorsByRequestedTime: '.$impactCasesForVentilatorsByRequestedTime.',
+            dollarsInFlight: '.$impactDollarsInFlight.'
+        },
+        severeImpact: {
+            currentlyInfected: '.$severeImpactCurrentlyInfected.',
+            infectionsByRequestedTime: '.$severeImpactInfectionsByRequestedTime.',
+            severeCasesByRequestedTime: '.$severeImpactSevereCasesByRequestedTime.',
+            hospitalBedsByRequestedTime: '.$severeImpactHospitalBedsByRequestedTime.',
+            casesForICUByRequestedTime: '.$severeImpactCasesForICUByRequestedTime.',
+            casesForVentilatorsByRequestedTime: '.$severeImpactCasesForVentilatorsByRequestedTime.',
+            dollarsInFlight: '.$severeImpactDollarsInFlight.'
+        }
+    }';
+
+    return $estimate;
+}
+
+function jaysonDecode($s)
+{
+    $s = str_replace(
+        array('"',  "'"),
+        array('"', '"'),
+        $s
+    );
+    $s = preg_replace('/(\w+):/i', '"\1":', $s);
+
+    return $s;
 }
