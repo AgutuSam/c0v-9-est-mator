078065f1526dc5e8ab4892261ae6476218aad973
AgutuSam <sammyhardcell@gmail.com>
2020-04-14 04:01:31 +0300

    jayess

--- src/estimator.php
+++ src/estimator.php
@@ -2,11 +2,11 @@
 
 function covid19ImpactEstimator($data)
 {
-    foreach ([$data] as $val) {
-        //Print out the element.
-        $stats = "'".$val."'";
-    }
-    $stats = preg_replace('/(\w+):/i', '"\1":', $stats);
+    echo '<script>
+    var json = JSON.stringify( '.$data.' );
+    </script>';
+    $stats = "<script type='text/javascript'>document.write(json)</script>";
+    // $stats = preg_replace('/(\w+):/i', '"\1":', $stats);
     // $stats = preg_replace('/(,|\{)[ \t\n]*(\w+)[ ]*:[ ]*/', '$1"$2":', $data);
     $stats = json_decode($stats, true);
 
